ed0e0b70a3cd10af1916fbeddeee081844303665
Brendan Weibrecht <brendan@weibrecht.net.au>
2015-03-05 11:18:15 +1100

    Add my Ruby aliases

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,3 +1,12 @@
 alias sc='script/console'
 alias sg='script/generate'
 alias sd='script/destroy'
+
+alias bi='bundle install'
+alias be='bundle exec'
+alias bu='bundle update'
+
+alias bers='bundle exec rails s'
+alias berc='bundle exec rails c'
+alias bets='bundle exec thin start'
+alias besh='bundle exec shotgun'
