a45de3994e4d6d05817c5f8d36d7bc8ff12af46a
Brendan Weibrecht <brendan@weibrecht.net.au>
2015-03-05 11:50:02 +1100

    Move Git functions into bin/

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -23,59 +23,15 @@ alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gds='git diff --staged'
 alias gdh='git diff HEAD'
 alias gpl='git pull'
-alias gundoc="git reset --soft 'HEAD^'"
+alias gundoc='git-undo'
 alias gclear='git reset --hard'
-alias gcop='git checkout HEAD~1'
-alias gcon='git_next'
 alias gsts='git stash save'
 alias gstp='git stash pop'
 alias gsta='git stash apply'
 alias gstl='git stash list'
 
-# Functions
-# ==============================================================================
-# TODO: move into own file
+alias gcop='git-checkout-commit-previous'
+alias gcon='git-checkout-commit-next'
 
-# Git stash & pull & pop stash
-# ------------------------------------------------------------------------------
-# Update from remote -- stash any changes, pull new commits, pop stash
-# TODO: test more thoroughly
-function gstpl {
-  echo "Stash save..."
-  git stash | grep "No local changes to save" > /dev/null
-  stashed=$?
-  test $stashed -eq 0 && echo "Nothing to stash"
-  test $stashed -ne 0 && echo "Stash saved"
-
-  echo "Pull..."
-  git pull
-
-  test $stashed -ne 0 && echo "Stash pop..." && git stash pop && echo "Stash pop'd"
-}
-
-# Git stash & pull & pop stash & commit
-# ------------------------------------------------------------------------------
-# Probably not a good idea to automate this, on second thought
-# TODO: test more thoroughly
-function gplc {
-  gstpl
-
-  echo "Commit..."
-  git commit "$@"
-  committed=$?
-
-  echo "Status..."
-  git status
-
-  test $committed -eq 0 && echo "Press Enter to push, or Ctrl+C to cancel" && read && git push
-}
-
-# Checkout next (newer) commit
-# ------------------------------------------------------------------------------
-# TODO: test more thoroughly
-git_next() {
-    BRANCH=`git show-ref | grep $(git show-ref -s -- HEAD) | sed 's|.*/\(.*\)|\1|' | grep -v HEAD | sort | uniq`
-    HASH=`git rev-parse $BRANCH`
-    PREV=`git rev-list --topo-order HEAD..$HASH | tail -1`
-    git checkout $PREV
-}
+alias gstpl='git-update-from-remote-using-stash'
+alias gpcl='git-update-from-remote-using-stash-then-commit'
