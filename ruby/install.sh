34d6d58d53b1883727773817df46ea6cc5b04e0d
Brendan Weibrecht <brendan@weibrecht.net.au>
2015-03-05 13:29:44 +1100

    Install Ruby using apt-get instead of brew

--- ruby/install.sh
+++ ruby/install.sh
@@ -3,11 +3,11 @@
 if test ! $(which rbenv)
 then
   echo "  Installing rbenv for you."
-  brew install rbenv > /tmp/rbenv-install.log
+  sudo apt-get install rbenv > /tmp/rbenv-install.log
 fi
 
 if test ! $(which ruby-build)
 then
   echo "  Installing ruby-build for you."
-  brew install ruby-build > /tmp/ruby-build-install.log
+  sudo apt-get install ruby-build > /tmp/ruby-build-install.log
 fi
