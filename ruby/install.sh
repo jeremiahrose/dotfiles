ebc3c1c465fffdd5f34a4b8576b9494b8206f9e6
Brendan Weibrecht <brendan@weibrecht.net.au>
2015-04-03 01:56:09 +1100

    Fix homebrew installation for linuxbrew; Make rbenv and ruby-build install through homebrew to have newer versions

--- ruby/install.sh
+++ ruby/install.sh
@@ -3,11 +3,11 @@
 if test ! $(which rbenv)
 then
   echo "  Installing rbenv for you."
-  sudo apt-get install rbenv > /tmp/rbenv-install.log
+  brew install rbenv > /tmp/rbenv-install.log
 fi
 
 if test ! $(which ruby-build)
 then
   echo "  Installing ruby-build for you."
-  sudo apt-get install ruby-build > /tmp/ruby-build-install.log
+  brew install ruby-build > /tmp/ruby-build-install.log
 fi
