15ce29c23892f14ca9b38b5a986c837bdbe8f271
Brendan Weibrecht <brendan@weibrecht.net.au>
2017-12-12 17:57:07 +1100

    Ruby: Remove '--user-install' from gemrc; Disable system Ruby paths (~/.gem)

--- ruby/path.zsh
+++ ruby/path.zsh
@@ -1,4 +1,4 @@
-export PATH=$HOME/.gem/bin:$PATH
-export PATH="$(ruby -e 'print Gem.user_dir')/bin:$PATH"
-export PATH=$HOME/.rbenv/shims:$PATH
+# export PATH=$HOME/.gem/bin:$PATH
+# System Ruby gems path
+# export PATH="$(ruby -e 'print Gem.user_dir')/bin:$PATH"
 export PATH=$HOME/.rbenv/bin:$PATH
