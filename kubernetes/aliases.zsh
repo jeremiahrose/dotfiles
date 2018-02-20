686ec0d5aa73b39a1022c18e15808a4a62da0c56
Brendan Weibrecht <brendan@weibrecht.net.au>
2018-02-20 15:32:27 +1100

    Use log_and_run_command for kubectl aliases

--- kubernetes/aliases.zsh
+++ kubernetes/aliases.zsh
@@ -1,12 +1,25 @@
-alias k='kubectl'
-alias kg='kubectl get'
-alias kgp='kubectl get pods'
-alias kgn='kubectl get nodes'
-alias kgs='kubectl get services'
-alias kgi='kubectl get ingresses'
-alias kd='kubectl describe'
-alias kdp='kubectl describe pods'
-alias kdn='kubectl describe nodes'
-alias kds='kubectl describe services'
-alias kdi='kubectl describe ingresses'
-alias ke='kubectl exec -it'
+function log_and_run_command {
+  args="$@"
+  echo -n "\e[34m"
+  echo -n '--> '
+  echo -n $args
+  echo "\e[39m"
+  eval $args
+}
+
+function k {
+  args="$@"
+  log_and_run_command kubectl $args
+}
+
+alias kg='k get'
+alias kgp='k get pods'
+alias kgn='k get nodes'
+alias kgs='k get services'
+alias kgi='k get ingresses'
+alias kd='k describe'
+alias kdp='k describe pods'
+alias kdn='k describe nodes'
+alias kds='k describe services'
+alias kdi='k describe ingresses'
+alias ke='k exec -it'
