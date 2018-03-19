f471bf086fc959bcb93d6343f8f361e02220ca59
Brendan Weibrecht <brendan@weibrecht.net.au>
2018-03-19 13:27:37 +1100

    Log result of z command, coloured

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -1,1 +1,12 @@
 source "$ZSH/oh-my-zsh/plugins/z/z.sh"
+unalias z
+function z {
+  if _z "$1" 2>&1; then
+    echo -n "\\e[34m" # blue
+    pwd
+  else
+    echo -n "\\e[31m" # red
+    echo "Directory match not found" >&2
+  fi
+  echo -n "\\e[39m"
+}
