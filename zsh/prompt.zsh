c8fb8ea7d877d1709fd6e6eac69b083183c4fa1b
ZimbiX <brendan@weibrecht.net.au>
2019-05-10 12:54:19 +1000

    Add gcloud and kube config to prompt; Use coloured prompt arrow to indicate last exit code

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -67,16 +67,48 @@ rb_prompt() {
   fi
 }
 
+gcloud_prompt() {
+  local cyan="\e[36m"
+  echo "${cyan}$(gcloud-project --current 2>&1)${reset_color}"
+}
+
+# https://github.com/jonmosco/kube-ps1
+if [ -f "$PROJECTS/kube-ps1/kube-ps1.sh" ]; then
+  . "$PROJECTS/kube-ps1/kube-ps1.sh"
+  KUBE_PS1_PREFIX=''
+  KUBE_PS1_SUFFIX=''
+  KUBE_PS1_SYMBOL_ENABLE=false
+  KUBE_PS1_CTX_COLOR=32
+
+  get_cluster_short() {
+    echo "$1" | sed -E 's/.+_//'
+  }
+
+  KUBE_PS1_CLUSTER_FUNCTION=get_cluster_short
+else
+  kube_ps1() {}
+fi
+
 directory_name() {
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
+exit_code_prompt() {
+  if [[ "$last_exit_code" == "0" ]]; then
+    local exit_code_prompt_colour="%{$fg_bold[green]%}"
+  else
+    local exit_code_prompt_colour="%{$fg_bold[red]%}"
+  fi
+  echo "${exit_code_prompt_colour}➜ ${reset_color}"
+}
+
+export PROMPT=$'\n$(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)\n$(gcloud_prompt) $(kube_ps1)\n$(exit_code_prompt)'
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
 
 precmd() {
+  last_exit_code="$?"
   title "zsh" "%m" "%55<...<%~"
   set_prompt
 }
