844d5ff2558099e9e4a84abafc9601d664337f8e
Brendan Weibrecht <brendan@weibrecht.net.au>
2019-06-06 23:39:28 +1000

    Source deX shell utils and stop sourcing the old set_dex_environment.sh; Default the deX config

--- greensync/dex.zsh
+++ greensync/dex.zsh
@@ -1,7 +1,18 @@
 scripts=(
-  $GOPATH/src/github.com/greensync/dex-deploy-tools/scripts/set_dex_environment.sh
+  # $GOPATH/src/github.com/greensync/dex-deploy-tools/scripts/set_dex_environment.sh
+  $PROJECTS/dex-core/scripts/shell-utils/dex-environments-vars.sh
+  $PROJECTS/dex-core/scripts/shell-utils/jq.sh
+  $PROJECTS/dex-core/scripts/shell-utils/jwt.sh
 )
 
 for file in $scripts; do
   [[ -s $file ]] && source $file
 done
+
+dex-db-dev
+dex-dev > /dev/null
+
+export VISION_DB_HOST="$DEX_DB_HOST"
+export VISION_DB_USER="$DEX_DB_USER"
+export VISION_DB_PASSWORD="$DEX_DB_PASSWORD"
+export VISION_DB_DATABASE=visibility_dev
