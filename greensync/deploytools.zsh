30b5f3beb5cd784a442f4bc4d616cb9c24c7b34c
Brendan Weibrecht <brendan@weibrecht.net.au>
2020-05-06 15:12:29 +1000

    Add alias for GreenSync's ./auto/with-deploytools pattern to forward my kube aliases, and colour the terminal prompt & prefix a blank line

---
+++ greensync/deploytools.zsh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wd() {
+  ./auto/with-deploytools bash -c "echo -e \"$( (cat ~/.dotfiles/kubernetes/aliases.zsh; printf 'PS1="\\n\\e[36;1m$PS1\\e[0m"') | base64)\" | base64 -d >> ~/.bashrc && bash"
+}
