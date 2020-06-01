fc41133c88220b5e9c5ccf7fa648d14e79a2fcb9
Brendan Weibrecht <brendan@weibrecht.net.au>
2020-06-01 13:12:24 +1000

    Clean up after yay

--- yay/aliases.zsh
+++ yay/aliases.zsh
@@ -1,2 +1,6 @@
 # Forces each AUR package's diff to be separately paginated on a clean screen, no matter the length
-alias yay='LESS=SR yay'
+# Also removes the (sometimes quite large) built packages afterwards which would otherwise gradually accumulate
+yay() {
+  LESS=SR command yay "$1" && \
+    rm -fv ~/.cache/yay/*/*.pkg.*
+}
