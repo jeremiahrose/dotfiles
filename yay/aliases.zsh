4659bb879d2ffcc283128739e2dfb49c04f7f60a
Brendan Weibrecht <brendan@weibrecht.net.au>
2020-06-23 13:09:57 +1000

    Fix passing arguments to yay

--- yay/aliases.zsh
+++ yay/aliases.zsh
@@ -1,6 +1,6 @@
 # Forces each AUR package's diff to be separately paginated on a clean screen, no matter the length
 # Also removes the (sometimes quite large) built packages afterwards which would otherwise gradually accumulate
 yay() {
-  LESS=SR command yay "$1" && \
+  LESS=SR command yay "$@" && \
     rm -fv ~/.cache/yay/*/*.pkg.*
 }
