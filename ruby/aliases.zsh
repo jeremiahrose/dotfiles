89a6ca0610527258abf8a96c63d322264905a1d3
Brendan Weibrecht <brendan@weibrecht.net.au>
2021-02-02 23:37:38 +1100

    Add alias to browse the source url of a gem

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -10,3 +10,19 @@ alias bers='bundle exec rails s'
 alias berc='bundle exec rails c'
 alias bets='bundle exec thin start'
 alias besh='bundle exec shotgun'
+
+gem-browse-source-url() {
+  (
+    set -Eeuo pipefail
+
+    local gem_info source_url
+    gem_info=$(curl -sSNf "https://rubygems.org/api/v1/gems/$1.json")
+    source_url=$(echo "$gem_info" | tr -d '\n' | jq -r .source_code_uri)
+    if [[ $source_url == null || $source_url == "" ]]; then
+      source_url=$(echo "$gem_info" | tr -d '\n' | jq -r .homepage_uri)
+    fi
+    echo "$source_url"
+    open "$source_url" &>/dev/null
+  )
+}
+alias gemb=gem-browse-source-url
