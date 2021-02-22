82adf1dcaf5abc6fc74e6dd78c7ecaea8ce5bbaa
Brendan Weibrecht <brendan@weibrecht.net.au>
2021-02-23 01:45:02 +1100

    wd: Allow non-interactive use of aliases

--- greensync/deploytools.zsh
+++ greensync/deploytools.zsh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-alias wd=./auto/with-deploytools
+add-docker-rc() {
+  grep -r -P '(?<=deploytools:)[^\{\$][^ "]+' -l | xargs grep .docker-rc.d -L | xargs -r -t perl -i -pe 's%^( +)(-w /work \\)%\1\2\n\1-v ~/.docker-rc.d/:/.docker-rc.d/:ro \\%g'
+}
+
+# Makes sure my Docker dotfiles get loaded in non-interactive mode too
+wd() {
+  add-docker-rc
+  ruby -r shellwords -e '
+    if ARGV.length == 0
+      c = "bash"
+    else
+      a = ARGV.shelljoin
+      b = "BASH_ENV=/root/.bashrc\ " + ["bash", "-c", a].shelljoin
+      c = ["bash", "-c", b].shelljoin
+    end
+    x = ["./auto/with-deploytools", c].join(" ")
+    puts "\e[34m--> " + x + "\e[39m"
+    system x
+  ' "$@"
+}
