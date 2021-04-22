81f1dbf64048f8e26da2c5b2efbad3a98b77eb79
Brendan Weibrecht <brendan@weibrecht.net.au>
2021-04-23 03:08:09 +1000

    Add dc-env function to produce a .env file from the Docker Compose environment config of the dev service

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -5,3 +5,8 @@ alias dcl='docker-clean'
 alias dcd='dc down'
 alias dcb='dc build'
 alias db='dc up -d db'
+
+# Produce a .env file from the Docker Compose environment config of the dev service
+dc-env() {
+  ruby -r yaml -e 'puts YAML.load_file(Dir.glob("docker-compose.y*ml")[0]).fetch("services").fetch("dev").fetch("environment").map { |k,v| val = v.to_s.gsub(/\${.*-(.*)}/, "\\1").gsub("/work/", ""); (val.length == 0 ? "# " : "") + [k, val].join("=") }.compact.join("\n")' >> .env
+}
