44e5a3a2338907b975a7042372739bf88de81c6e
Brendan Weibrecht <brendan@weibrecht.net.au>
2021-06-09 12:13:22 +1000

    Make gcrlt support either image name or full image name; Add gcrl

--- gcloud/aliases.zsh
+++ gcloud/aliases.zsh
@@ -1,5 +1,19 @@
 alias gcrli='gcloud container images list --repository=asia.gcr.io/greensync-container-registry'
 
 gcrlt() {
-  gcloud container images list-tags asia.gcr.io/greensync-container-registry/"$1"
+  local full_image_name
+  if [[ "$1" =~ ^asia.gcr.io/greensync-container-registry/ ]]; then
+    full_image_name="$1"
+  else
+    full_image_name="asia.gcr.io/greensync-container-registry/$1"
+  fi
+  gcloud container images list-tags "$full_image_name" --sort-by=TIMESTAMP
+}
+
+gcrl() {
+  if [[ -n "$1" ]]; then
+    gcrlt "$1"
+  else
+    gcrli
+  fi
 }
